774579f11a8109aaabdfed7e87992abbdf50063f
temp-9384289 <asdjfkasljd512561464325345@gmail.com>
2024-02-12 23:10:48 -0500

    up

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flask-ngrok-CART",
-    version="0.0.0",
+    name="cart-flask-ngrok",
+    version="0.0.1",
     author="Nathan Reitinger",
     description="An offshoot of flask-ngrok for demo Flask apps using ngrok.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/nathanReitinger/flask-ngrok-cart",
+    url="",
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: Apache Software License",
@@ -18,5 +18,5 @@ setuptools.setup(
     ],
     keywords='flask ngrok CART',
     install_requires=['Flask>=0.8', 'requests'],
-    py_modules=['flask_ngrok_CART']
+    py_modules=['cart_flask_ngrok']
 )
